268dbe628049f1e30db5e2f4cf71e98966f659c2
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-02-28 01:00:46 -0500

    Create __init__.py

---
+++ src/parameter_generators/__init__.py
@@ -0,0 +1,19 @@
+import enum
+from src.parameter_generators.grid_search import GridHpSearch
+from src.parameter_generators.random_search import RandomHpSearch
+from src.parameter_generators.gp_search import GPOHpSearch
+
+
+class SearchType(enum.Enum):
+    __order__ = "Grid Random GPO"
+    # __order__ = "Grid Random"
+    Grid = 0
+    Random = 1
+    GPO = 2
+
+
+search_type_2_type = {
+    SearchType.Grid: GridHpSearch,
+    SearchType.Random: RandomHpSearch,
+    SearchType.GPO: GPOHpSearch,
+}
