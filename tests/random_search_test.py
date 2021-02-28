d7a91d942ca1183a2885ba2dc7e65571a582ed18
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-02-28 01:01:50 -0500

    Update random_search_test.py

--- tests/random_search_test.py
+++ tests/random_search_test.py
@@ -1,6 +1,6 @@
 import unittest
 import time
-from src.random_search import RandomHpSearch
+from src.parameter_generators.random_search import RandomHpSearch
 
 
 class TestRandomHpSearch(unittest.TestCase):
@@ -9,6 +9,19 @@ class TestRandomHpSearch(unittest.TestCase):
         self.assertEqual(bool(rnGen), True)
         time.sleep(3)
         self.assertEqual(bool(rnGen), False, f"gen elapse time: {rnGen.elapse_time}")
+
+    def test_increment_counters(self):
+        rnGen = RandomHpSearch({"x0": range(0, 10)}, max_itr=1)
+        self.assertEqual(rnGen.current_itr, 0)
+        self.assertEqual(rnGen.elapse_time_per_iteration, {})
+        time.sleep(0.5)
+        rnGen.get_trial_param()
+
+        self.assertEqual(rnGen.current_itr, 1, f"rnGen.current_itr: {rnGen.current_itr}")
+        self.assertTrue(rnGen.last_itr_elapse_time > 0, f"rnGen.last_itr_elapse_time: {rnGen.last_itr_elapse_time}")
+        self.assertTrue(rnGen.elapse_time_per_iteration[rnGen.current_itr-1] > 0,
+                        f"rnGen.elapse_time_per_iteration[rnGen.current_itr]: "
+                        f"{rnGen.elapse_time_per_iteration[rnGen.current_itr-1]}")
 
 
 if __name__ == '__main__':
