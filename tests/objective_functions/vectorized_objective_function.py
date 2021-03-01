3a14af899ceb27ab6a033e100f81d907554fe8ba
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-02-28 21:06:18 -0500

    Update vectorized_objective_function.py

--- tests/objective_functions/vectorized_objective_function.py
+++ tests/objective_functions/vectorized_objective_function.py
@@ -34,12 +34,13 @@ def vectorized_objective_function(x, **kwargs):
 
 class VectorizedObjectiveFuncHpOptimizer(HpOptimizer):
     dim = 2
+    nb_points = 100_000
     hp_space = None
     params = None
     meshgrid = None
     max: float = 1.0
     min: float = 0.0
-    space = None
+    score_space = None
 
     def set_dim(self, new_dim: int):
         self.dim = new_dim
@@ -47,16 +48,17 @@ class VectorizedObjectiveFuncHpOptimizer(HpOptimizer):
     def build_model(self, **hp) -> Callable:
         if self.meshgrid is None:
             self.hp_space = {
-                f"x{i}": np.linspace(-1, 1, 100) for i in range(self.dim)
+                f"x{i}": np.linspace(-1, 1, int(np.floor(self.nb_points**(1/self.dim)))) for i in range(self.dim)
             }
             self.params = dict(
-                omega=np.random.normal(10.0, 1.0, self.dim),
+                # omega=np.random.normal(10.0, 1.0, self.dim),
+                omega=np.array([1*10**(i/self.dim+1) for i in range(self.dim)]),
                 noise=0.0,
             )
             self.meshgrid = np.meshgrid(*[self.hp_space[f"x{i}"] for i in range(self.dim)])
-            self.space = vectorized_objective_function(np.array(self.meshgrid), **self.params)
-            self.max = np.max(self.space)
-            self.min = np.min(self.space)
+            self.score_space = vectorized_objective_function(np.array(self.meshgrid), **self.params)
+            self.max = np.max(self.score_space)
+            self.min = np.min(self.score_space)
         return vectorized_objective_function
 
     def fit_model_(self,
@@ -86,7 +88,7 @@ if __name__ == '__main__':
 
     # ----------------- Initialization -------------------- #
     obj_func_hp_optimizer = VectorizedObjectiveFuncHpOptimizer()
-    obj_func_hp_optimizer.set_dim(3)
+    obj_func_hp_optimizer.set_dim(2)
     func = obj_func_hp_optimizer.build_model()
     print(f"Maximum of objective function: {obj_func_hp_optimizer.max:.3f}")
     Z, _ = obj_func_hp_optimizer.score(
@@ -94,8 +96,8 @@ if __name__ == '__main__':
         **{f"x{i}": obj_func_hp_optimizer.meshgrid[i] for i in range(obj_func_hp_optimizer.dim)}
     )
 
-    print(f"Z.shape: {Z.shape}")
-    print(f"Z[0, 1].shape: {Z[:, :, 0].shape}")
+    # print(f"Z.shape: {Z.shape}")
+    # print(f"Z[0, 1].shape: {Z[:, :, 0].shape}")
 
     # ----------------- Figure -------------------- #
     fig = go.Figure()
@@ -104,17 +106,20 @@ if __name__ == '__main__':
         go.Surface(
             x=obj_func_hp_optimizer.hp_space["x0"],
             y=obj_func_hp_optimizer.hp_space["x1"],
-            z=Z[:, :, 0],
+            z=Z[:, :, ],
         )
     )
     fig.update_traces(contours_z=dict(show=True, usecolormap=True,
                                       highlightcolor="limegreen", project_z=True))
     fig.update_layout(
-        title='Vectorized objective function',
+        title=f'{obj_func_hp_optimizer.dim}D vectorized objective function',
         autosize=True,
         margin=dict(t=150, b=150, l=150, r=150),
         template="seaborn",
     )
+    fig.update_xaxes(title=f"x0: parameter sscore_space [-]")
+    fig.update_yaxes(title=f"x1: parameter sscore_space [-]")
+    # fig.update_zaxes(title="Score [-]")
 
     # Add dropdown
     fig.update_layout(
@@ -130,8 +135,8 @@ if __name__ == '__main__':
                             ),
                             {
                                 # "title": f"{xi}-{xj}",
-                                "xaxis.title.text": f"{xi}: parameter space [-]",
-                                "yaxis.title.text": f"{xj}: parameter space [-]",
+                                "xaxis.title.text": f"{xi}: parameter sscore_space [-]",
+                                "yaxis.title.text": f"{xj}: parameter sscore_space [-]",
                                 "zaxis.title.text": "Score [-]",
                             }
                         ],
@@ -151,21 +156,22 @@ if __name__ == '__main__':
         ]
     )
 
-    save_dir = f"../figures/vectorized_objective_function/"
+    save_dir = f"figures/vectorized_objective_function/"
     os.makedirs(save_dir, exist_ok=True)
     fig.write_html(f"{save_dir}/{'-'.join([f'{k}_{v}' for k, v in obj_func_hp_optimizer.params.items()])}.html")
-    # fig.show()
+    fig.show()
 
     # ----------------- Optimization -------------------- #
     start_time = time.time()
 
-    param_gen = GPOHpSearch(obj_func_hp_optimizer.hp_space, max_itr=10_000, max_seconds=60*15)
+    param_gen = GPOHpSearch(obj_func_hp_optimizer.hp_space, max_seconds=60*1)
 
     opt_param_gen = obj_func_hp_optimizer.optimize(
         param_gen,
         np.ones((2, 2)),
         np.ones((2, 2)),
-        n_splits=2
+        n_splits=2,
+        stop_criterion=0.95,
     )
 
     opt_hp = opt_param_gen.get_best_param(from_history=True)
