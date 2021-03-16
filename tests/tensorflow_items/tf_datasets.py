8d6e3064f5e0c1f4d1936d67008ee955eafa202a
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-03-16 18:20:04 -0400

    Create tf_datasets.py

---
+++ tests/tensorflow_items/tf_datasets.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+def get_tf_mnist_dataset(**kwargs):
+    # https://www.tensorflow.org/datasets/keras_example
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+
+    # Build training pipeline
+    ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_train = ds_train.batch(128)
+    ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+    # Build evaluation pipeline
+    ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_test = ds_test.batch(128)
+    ds_test = ds_test.cache()
+    ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+    return ds_train, ds_test
+
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
