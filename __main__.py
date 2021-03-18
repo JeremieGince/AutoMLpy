a130b1e3af4125aed1a51fdaf783480614c8f377
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-03-18 00:51:15 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,2 +1,8 @@
+from src import logging_tools
+import logging
+
 if __name__ == '__main__':
-    pass
+    logging_tools.logs_file_setup(__file__, level=logging.INFO)
+    logging_tools.log_device_setup()
+    logging.info(r"More info about the package at: https://github.com/JeremieGince/AutoMLpy")
+
