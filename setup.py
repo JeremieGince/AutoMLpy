300023c2d8a113610f0bd9db55b8a5aeddf348f3
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-03-18 00:21:48 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='AutoMLpy',
+    version='0.0.1',
+    description="This package is an automatic machine learning module whose function"
+                " is to optimize the hyper-parameters of an automatic learning model.",
+    url='https://github.com/JeremieGince/AutoMLpy',
+    author='Jérémie Gince',
+    author_email='gincejeremie@gmail.com',
+    license='MIT',
+    packages=['AutoMLpy'],
+    zip_safe=False,
+)
