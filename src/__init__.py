bf3af3c20daabad88b61dffde77f840d90c8e01c
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-03-17 22:44:53 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
 from src.logging_tools import logs_file_setup, log_device_setup, DeepLib
+from src.optimizers import HpOptimizer
+from src.parameter_generators.grid_search import GridHpSearch
+from src.parameter_generators.random_search import RandomHpSearch
+from src.parameter_generators.gp_search import GPOHpSearch
