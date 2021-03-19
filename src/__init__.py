293c0c359847d558be36a8d1734b788f04e6da96
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-03-19 16:11:42 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
+__author__ = "Jérémie Gince"
+
 from src.logging_tools import logs_file_setup, log_device_setup, DeepLib
 from src.optimizers import HpOptimizer
 from src.parameter_generators.grid_search import GridHpSearch
 from src.parameter_generators.random_search import RandomHpSearch
 from src.parameter_generators.gp_search import GPOHpSearch
+
+
