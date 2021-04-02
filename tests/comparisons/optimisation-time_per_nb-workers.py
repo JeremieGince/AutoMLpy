659986ab13197bbbcecbc06eb87204b01b31274e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-04-02 15:54:40 -0400

    Update optimisation-time_per_nb-workers.py

--- tests/comparisons/optimisation-time_per_nb-workers.py
+++ tests/comparisons/optimisation-time_per_nb-workers.py
@@ -1,13 +1,20 @@
-from src.AutoMLpy import SearchType
+from src.AutoMLpy.parameter_generators import SearchType
 from src.AutoMLpy import logs_file_setup, log_device_setup
 from tests import execute_optimisation
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import os
-from src.AutoMLpy import plotly_colors
+from src.AutoMLpy.tools import plotly_colors
 import logging
 import multiprocessing
+import tracemalloc
+
+try:
+    import tensorflow as tf
+    tf.get_logger().setLevel(logging.FATAL)
+except ImportError:
+    pass
 
 
 def compute_stats_per_workers_table(
@@ -19,22 +26,22 @@ def compute_stats_per_workers_table(
 ):
     algo_types = kwargs.get("algos", SearchType)
     columns = ["Workers", *[st.name for st in algo_types]]
+
     iterations_results = pd.DataFrame(columns=columns)
     time_results = pd.DataFrame(columns=columns)
-
-    # TODO: get memory usage https://stackoverflow.com/questions/938733/total-memory-used-by-python-process
     memory_results = pd.DataFrame(columns=columns)
 
     for w in range(1, max_workers+1):
         logging.info(f"\n{'-'*50} {w} Workers {'-'*50}")
         new_iterations_results = {"Workers": w, **{st.name: [] for st in algo_types}}
         new_time_results = {"Workers": w, **{st.name: [] for st in algo_types}}
+        new_memory_results = {"Workers": w, **{st.name: [] for st in algo_types}}
 
         for _search_type in algo_types:
             logging.info(f"\n{'-'*10} {_search_type.name} search {'-'*10}")
-            ell_itr = []
-            ell_time = []
+            ell_itr, ell_time, ell_mem = [], [], []
             for itr_seed in range(iterations_per_workers):
+                tracemalloc.start()
                 param_gen = execute_optimisation(
                     _search_type,
                     dim=dim,
@@ -44,16 +51,23 @@ def compute_stats_per_workers_table(
                     seed=itr_seed,
                     **kwargs
                 )
+                current_mem, peak_mem = tracemalloc.get_traced_memory()
+
                 ell_itr.append(param_gen.current_itr)
                 ell_time.append(param_gen.last_itr_elapse_time)
+                ell_mem.append(peak_mem * 1e-6)  # convert bytes to MB
+
+                tracemalloc.stop()
 
             new_iterations_results[_search_type.name] = (np.mean(ell_itr), np.std(ell_itr))
             new_time_results[_search_type.name] = (np.mean(ell_time), np.std(ell_time))
+            new_memory_results[_search_type.name] = (np.mean(ell_mem), np.std(ell_mem))
 
         iterations_results = iterations_results.append(new_iterations_results, ignore_index=True)
         time_results = time_results.append(new_time_results, ignore_index=True)
+        memory_results = memory_results.append(new_memory_results, ignore_index=True)
 
-    return iterations_results, time_results
+    return iterations_results, time_results, memory_results
 
 
 def show_stats_per_dimension(
@@ -64,7 +78,7 @@ def show_stats_per_dimension(
         **kwargs
 ):
 
-    iterations_results, time_results = compute_stats_per_workers_table(
+    iterations_results, time_results, memory_results = compute_stats_per_workers_table(
         max_workers, dim, iterations_per_workers, compute_delay, **kwargs
     )
     keys = [st.name for st in kwargs.get("algos", SearchType)]
@@ -87,8 +101,18 @@ def show_stats_per_dimension(
         for st in keys
     }
 
+    memory_results_mean = {
+        st: np.array([x[0] for x in memory_results[st]])
+        for st in keys
+    }
+    memory_results_std = {
+        st: np.array([x[1] for x in memory_results[st]])
+        for st in keys
+    }
+
     iterations_y_list = []
     time_y_list = []
+    memory_y_list = []
 
     # --------------------------------------------------------------------------------- #
     #                              Initialize figure                                    #
@@ -108,6 +132,11 @@ def show_stats_per_dimension(
         time_std_lower = list(time_results_mean[st] - time_results_std[st])
         time_mean = list(time_results_mean[st])
         time_std = time_std_lower + time_std_upper[::-1]
+
+        memory_std_upper = list(memory_results_mean[st] + memory_results_std[st])
+        memory_std_lower = list(memory_results_mean[st] - memory_results_std[st])
+        memory_mean = list(memory_results_mean[st])
+        memory_std = memory_std_lower + memory_std_upper[::-1]
 
         fig.add_trace(
             go.Scatter(x=x,
@@ -132,6 +161,9 @@ def show_stats_per_dimension(
 
         time_y_list.append(time_mean)
         time_y_list.append(time_std)
+
+        memory_y_list.append(memory_mean)
+        memory_y_list.append(memory_std)
 
     fig.update_xaxes(title=f"Workers")
     fig.update_yaxes(title="Iterations [-]")
@@ -169,8 +201,8 @@ def show_stats_per_dimension(
                         method="update"
                     )
                     for y_list, label in zip(
-                        [iterations_y_list, time_y_list],
-                        ["Iterations [-]", "Time [s]"]
+                        [iterations_y_list, time_y_list, memory_y_list],
+                        ["Iterations [-]", "Time [s]", "Memory [MB]"]
                     )
                 ]),
                 direction="down",
@@ -194,21 +226,20 @@ def show_stats_per_dimension(
                        f"-maxworkers{max_workers}-dim{dim}-iteration{iterations_per_workers}.html")
 
     fig.show()
-    return iterations_results, time_results
+    return iterations_results, time_results, memory_results
 
 
 if __name__ == '__main__':
     logs_file_setup(__file__, level=logging.INFO)
     log_device_setup()
 
-    iterations_results, time_results = show_stats_per_dimension(
-        max_workers=min(10, multiprocessing.cpu_count()//2),
-        # max_workers=3,
+    iterations_results, time_results, memory_results = show_stats_per_dimension(
+        max_workers=min(5, multiprocessing.cpu_count()//2),
         dim=1,
-        iterations_per_workers=10,
-        compute_delay=0.05,
-        stop_criterion=0.9,
-        algos=[SearchType.Random, SearchType.GPO],
+        iterations_per_workers=2,
+        compute_delay=0.1,
+        stop_criterion=0.75,
+        algos=[SearchType.GPO, ],
         dark_mode=False
     )
 
@@ -234,5 +265,17 @@ if __name__ == '__main__':
 
     logging.info('\n' + ('-' * 125) + '\n' + "Time per Workers results LaTex" + '\n' + ('-' * 125))
     logging.info(time_results.to_latex())
+    logging.info(('-' * 125) + '\n')
+
+    # --------------------------------------------------------------------------------- #
+    #                           Memory per Workers results                              #
+    # --------------------------------------------------------------------------------- #
+
+    logging.info('\n' + ('-' * 125) + '\n' + "Memory per Workers results" + '\n' + ('-' * 125))
+    logging.info(memory_results)
+    logging.info(('-' * 125) + '\n')
+
+    logging.info('\n' + ('-' * 125) + '\n' + "Memory per Workers results LaTex" + '\n' + ('-' * 125))
+    logging.info(memory_results.to_latex())
     logging.info(('-' * 125) + '\n')
 
