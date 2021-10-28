f19bf9892699663ae07afbf136e5ef6ca7fe3fbf
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-10-27 21:46:04 -0400

    Update __init__.py

--- src/AutoMLpy/parameter_generators/__init__.py
+++ src/AutoMLpy/parameter_generators/__init__.py
@@ -2,18 +2,21 @@ import enum
 from .grid_search import GridHpSearch
 from .random_search import RandomHpSearch
 from .gp_search import GPOHpSearch
+from .forest_search import RandomForestEpsilonGreedySearch
 from .parameter_generator import ParameterGenerator
 
 
 class SearchType(enum.Enum):
-    __order__ = "Grid Random GPO"
+    __order__ = "Grid Random GPO Forest"
     Grid = 0
     Random = 1
     GPO = 2
+    Forest = 3
 
 
 search_type_2_type = {
     SearchType.Grid: GridHpSearch,
     SearchType.Random: RandomHpSearch,
     SearchType.GPO: GPOHpSearch,
+    SearchType.Forest: RandomForestEpsilonGreedySearch,
 }
